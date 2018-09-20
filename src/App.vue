27d74082ff8ca234a3ef1f88b95338ec0ed074e3
Sergey Nagornykh <serega1905@ukr.net>
2018-09-20 15:05:09 +0300

    vue cli, materialize ui, add navbar

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+
+    <nav class="blue darken-2">
+      <div class="nav-wrapper">
+        <a href="#" class="brand-logo">Vue app</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <router-link tag="li" to="/" exact>
+            <a>Home</a>
+          </router-link>
+          <router-link tag="li" to="/about" exact>
+            <a>About</a>
+          </router-link>
+        </ul>
+      </div>
+    </nav>
+
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.brand-logo {
+  left: 10px;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <button class="waves-effect waves-light btn-small">Button</button>
+  </div>
+</template>
